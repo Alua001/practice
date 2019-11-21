89b121be6bf23222ee573b6f53fb13f811fc277a
Alua001 <57936594+Alua001@users.noreply.github.com>
2019-11-21 12:32:57 +0600

    Add files via upload

---
+++ task100.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+bool ifHasDThree(int n){
+	while (n > 0){
+		if (n % 10 == 5) 
+			return false;
+		if (n % 10 == 6) 
+			return false;
+		n /= 10;
+	}
+	return true;
+}
+int main(){
+	cout << "Target numbers:" << "\n";
+	for (int i = 1000; i <= 9999; i++){
+		if (ifHasDThree(i))
+			cout << i << " ";
+	}
+	cout << "\n";
+	return 0;
+}
