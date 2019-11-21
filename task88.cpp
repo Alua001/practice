1465aee15fbbb6bc94ee9598851cda0af57ad897
Alua001 <57936594+Alua001@users.noreply.github.com>
2019-11-21 12:23:15 +0600

    Add files via upload

---
+++ task88.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int sum = 1;
+    for(int i = 5; i <= 13; i++){
+        sum *= i;
+    }
+    cout << sum << endl;
+    return 0;
+}
---
+++ task94.cpp
@@ -0,0 +1,13 @@
+#include <iostream>                    
+using namespace std;
+int main() {
+	double sum = 0;
+	int n;
+	cin >> n;
+	for (double i = 0; i < n; i++) {
+		sum = sum + 1 / (i + 1);
+
+	}
+	cout << sum << endl;
+	return 0;
+}
